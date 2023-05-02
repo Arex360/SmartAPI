fd50348bfe6d1dc60b1fbcb04d506d305662bb40
Arex360 <49303647+Arex360@users.noreply.github.com>
2023-05-02 20:42:08 +0500

    update

--- chart.py
+++ chart.py
@@ -18,14 +18,34 @@ with open(args.filename, 'r') as f:
 dates = [int(d['date']) for d in data]
 temps = [float(d['hum'])/10 for d in data]
 hums = [float(d['temp'])/10 for d in data]
+# Create subplots for temperature and humidity
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
 
-plt.bar(dates, temps, label='Temperature')
-plt.bar(dates, hums, label='Humidity')
+# Plot temperature data
+ax1.bar(dates, temps, label='Temperature')
+ax1.set_ylabel('Temperature')
 
+# Plot humidity data
+ax2.bar(dates, hums, label='Humidity')
+ax2.set_ylabel('Humidity')
+
+# Set x-axis label and tick marks
 plt.xlabel('Date')
 plt.xticks(range(min(dates), max(dates)+1))
-plt.ylabel('Value')
-plt.legend()
+
+# Add legend to each subplot
+ax1.legend()
+ax2.legend()
 
 #plt.show()
 plt.savefig(f"{args.filename}.png")
+#plt.bar(dates, temps, label='Temperature')
+#plt.bar(dates, hums, label='Humidity')
+
+#plt.xlabel('Date')
+#plt.xticks(range(min(dates), max(dates)+1))
+#plt.ylabel('Value')
+#plt.legend()
+
+#plt.show()
+#plt.savefig(f"{args.filename}.png")
